9b42fd5b73f9e8b144ddf2ed543ddcafe0e280db
Aleksandr Ivanov <aivanov@hq.fix.ru>
2019-08-29 10:14:24 +0300

    added getting call from get param json

---
+++ src/components/plugins/calls/PluginCallGetJson.php
@@ -0,0 +1,24 @@
+<?php
+namespace extas\components\plugins\calls;
+
+use extas\components\plugins\Plugin;
+
+/**
+ * Class PluginCallGetJson
+ *
+ * @package extas\components\plugins\alice
+ * @author jeyroik@gmail.com
+ */
+class PluginCallGetJson extends Plugin
+{
+    /**
+     * @param $yandexCall
+     */
+    public function __invoke(&$yandexCall)
+    {
+        if (isset($_REQUEST['json'])) {
+            $get = json_decode($_REQUEST['json'], true);
+            $get && $yandexCall = $get;
+        }
+    }
+}
